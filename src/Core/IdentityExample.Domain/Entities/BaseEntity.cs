f9ce52f5318756e783d3b1b1acd3e26fee0b0149
Dinçer Şipka <dincersipka6@gmail.com>
2023-06-14 13:26:28 +0300

    Entities Created.

---
+++ src/Core/IdentityExample.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace IdentityExample.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        public string? DeletedBy { get; set; }
+        public string? DeleteDate { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ src/Core/IdentityExample.Domain/Entities/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityExample.Domain.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
